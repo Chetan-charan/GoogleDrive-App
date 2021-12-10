5b12446882d6f4cbdc86dbb9a199ba4b1b3733cb
Chetan-charan <chetanharicharan@gmail.com>
2021-12-10 19:51:01 +0530

    commit

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ dotenv.config();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     next();
 });
 
